9c9102fcb11063ed4c8cd6374e764d5fdbf45fa5
Hamdaan42 <71827478+Hamdaan42@users.noreply.github.com>
2020-11-01 19:10:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -38,7 +38,7 @@ if(hasCollided(bullet,wall))
 {
   bullet.velocityX=0;
   var damage=0.5 *weight * speed *speed/(thickness *thickness*thickness);
-  |
+  
  if(damage>10)
  {
    wall.shapeColor=color(255,0,0);
